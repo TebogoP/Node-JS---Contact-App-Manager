2bfe011f1e9c5388d4e569f8af28c12b26c32b20
Tebogo <tphiri.firi@gmail.com>
2026-09-06 09:16:31 +0200

    Added a get request

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 
+//Helps with getting things from .env files
 const dotenv = require("dotenv");
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 
+// Middleware
+app.use(express.json());
 
+//Gets all contacts
+app.get("/api/contacts", (req, res) => {
+    res.send("Get all Contacts ");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
