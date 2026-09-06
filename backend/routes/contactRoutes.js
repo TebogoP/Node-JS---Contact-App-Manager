6dac6fcf5803a01284d298494e371848614ee4e6
Tebogo <tphiri.firi@gmail.com>
2026-09-06 09:33:12 +0200

    Added CRUD routes

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -5,4 +5,18 @@ router.get("/", (req, res) => {
     res.status(200).json({"message":"Get all Contacts from contactRoutes "});
 });
 
+router.get("/:id", (req, res) => {
+    res.status(200).json({"message": `Get a Contact id:  ${req.params.id} from contactRoutes `});
+});
+router.post("/", (req, res) => {
+    res.status(201).json({"message":"Create a new Contact from contactRoutes "});
+});
+
+router.patch("/:id", (req, res) => {
+    res.status(200).json({"message": `Update a Contact id:  ${req.params.id} from contactRoutes `});
+});
+router.delete("/:id", (req, res) => {
+    res.status(200).json({"message": `Delete a Contact id:  ${req.params.id} from contactRoutes `});
+});   
+        
 module.exports = router;
